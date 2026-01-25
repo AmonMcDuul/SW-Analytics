fddb6a6887cf489051427c62f438500181fa1c87
AmonMcDuul <schols.joran@gmail.com>
2026-01-25 01:18:57 +0100

    oninit

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AnalyticsStore } from '../../store/analytics.store';
 import { CommonModule } from '@angular/common';
 
@@ -8,10 +8,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   store = inject(AnalyticsStore);
 
-  constructor() {
+  ngOnInit(){
     this.store.load();
   }
 }
